c717a729c02c404f172fb345b98cd81f7d74b344
Elai <etrpekson2014@plm.edu.ph>
2018-05-11 12:57:53 +0800

    added dev keys

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: "387777075097-gqqe0k5ij2grdetld69o07q3e1gk4amc.apps.googleusercontent.com",
+    googleClientSecret: "v1aQPATt_Ba0etO5M5PKtjtb",
+    mongoURI: "mongodb://elai:password@ds217350.mlab.com:17350/breddit-dev",
+    cookieKey: 'asdasdasddczxxcvxcbcbkcvjlbcvklbjlbjcvklbjcvklbj',
+};  
